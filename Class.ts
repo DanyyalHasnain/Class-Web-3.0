fc14e440752f5c4cad9b3d6148e3f732f3090174
Danyyal Hassnain <125298765+DanyyalHasnain@users.noreply.github.com>
2023-02-21 11:18:19 +0500

    Add files via upload

---
+++ Class.ts
@@ -0,0 +1,21 @@
+//console.log('Hello World');
+
+
+/*let dividend = 11
+let Devisor = 2
+let Quotient = 5
+let remainder = dividend - (Devisor * Quotient)
+console.log(remainder);
+
+console.log(dividend % Devisor);*/
+
+/*let result;
+let num1 = 11;
+let num2 = 2;
+result = num1 % num2;
+console.log(result);*/
+
+
+/*let string1 = 'Sir Zia is Handsome';
+let splits1 = string1.split(" ");
+console.log(splits1);*/
