aa9c270508ba35feb67455de7470703fd659bc12
Danyyal Hassnain <125298765+DanyyalHasnain@users.noreply.github.com>
2023-02-28 13:29:30 +0500

    Add files via upload

---
+++ Classes/class 26Feb.ts
@@ -0,0 +1,73 @@
+/*let myName: String = 'Danyyal';
+let newNumber: number = 5;
+let bol:boolean = true
+console.log(bol);
+console.log(myName);
+myName = "how";
+console.log(myName);
+
+let wer = true;
+wer = false;
+
+
+{
+    let wer = 3
+    console.log(wer);
+}
+console.log(wer);
+
+let empty = 'Akbar';
+let ages1 = 34;
+
+console.log(`My name is ${empty} and his age is ${ages1}`);*/
+
+/*let n1 = 1;
+let n2 = 2;
+console.log(n1+n2);
+
+let str1 ='1';
+let str2 ='2';
+console.log(str1+str2);*/
+
+/*let no = 5;
+no++// add 1 in value
+no=no+1// add 1 in value
+no+=7//add 7 into 5
+no--//subtract 1 from value
+no=no-1//subtract 1 from value
+no-=7//subtract 7 from value
+
+console.log(no);*/
+
+/*let ke = 5
+let me = 5
+
+console.log(ke == me);//compare value
+console.log(ke === me);//compare value & type
+console.log(ke != me);//not equal
+//&& and consition, ||or condition*/
+
+
+/*function greet(naMe:string){
+
+    console.log(`hello ${naMe}`)
+}
+
+greet("Danyyal");*/
+
+/*function sum(p1:number , p2:number){
+    return p1+p2
+    
+}
+console.log(sum(3,5));*/
+
+
+
+
+
+const sum=(n1:number, n2:number):number=>{
+
+return n1+n2;
+}
+
+console.log(sum(3,4));
