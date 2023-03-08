6d7db140efe58fe2d1839816e2f418248e3388cc
Danyyal Hassnain <125298765+DanyyalHasnain@users.noreply.github.com>
2023-03-08 23:29:54 +0500

    Add files via upload

---
+++ Classes/Online08Mar.ts
@@ -0,0 +1,130 @@
+/*function calculateAverage(numm1:number, numm2:number):number {
+    return (numm1+numm2)/2;
+}
+let avg = calculateAverage(30,60);
+console.log(avg);
+
+
+
+
+function calculateAverage2 (){
+    console.log()
+}
+let avg3 = calculateAverage2();
+console.log(avg3);
+
+
+
+let avg2 = (numm1:number, numm2:number) => {
+  let myAvg = numm1/numm2
+    return myAvg;
+};
+
+
+
+
+let zzz = (numm1:number, numm2:number) => {
+    return (numm1+numm2)/2;
+}
+let avg = calculateAverage(30,60);
+console.log(avg);*/
+
+
+
+
+/*let f3 = (age:number, name:string) =>{
+    return `Mr. ${name}, Your age is ${age}`;
+}
+
+console.log(f3(24, 'Daniyal'));*/
+
+
+
+
+/*function optparams(param1:number, param2?:string,param3:number=5){
+   // console.log(`${param1}, ${param2}, ${param3}`);
+    return `${param1}, ${param2}, ${param3}`;
+}
+
+let xyz = optparams(10, 'Alfa',20);
+console.log(xyz);
+
+let xyz2 = optparams(10,"beta");
+console.log(xyz2);
+
+let xyz3 = optparams(10);
+console.log(xyz3);*/
+
+
+
+
+/*let x = Math.random()*10;
+
+if (x > 5) {
+    console.log(`The value of X > ${x}`);   
+}
+else
+{
+    console.log(`The value of X <= ${x}`);
+}*/
+
+
+
+
+
+
+/*let x = Math.round(Math.random()*10);
+
+if (x > 0 && x <= 3) {
+    console.log(`The value of ${x} >0 and <=3`);   
+}else if (x >= 3 && x <= 6)
+{
+    console.log(`The value of ${x} >= 4 and <=6`);
+}else if (x >= 7 && x <= 9){
+    console.log(`The value of ${x} >= 7 and <=9`);
+}else{
+    console.log(`The value of ${x} >= 9 and <=10`);
+}*/
+
+
+
+
+
+
+
+//let index = Math.random()*10; //have to generate no only 0,1,2
+
+
+// Ticketing system
+/*let country = ['Pakistan','Turkey','Canada'];
+let age = Math.round(Math.random()*90);
+
+if (country [0] === "Pakistan") {
+    if (age >= 18) {
+        console.log("Here is your ticket")
+    } else {
+        console.error("Age restriction")
+    }
+}else if (country [0] === "Turkey"){
+    if (age >= 30) {
+        console.log("Here is your ticket")
+    } else {
+        console.error("Age restriction")
+    }
+}
+else {
+    console.log("Invalid country")
+};*/
+
+
+/*let numbers = [21,34,42,36,15,87,23,56,90,34];
+console.log(numbers[7]);
+console.log(numbers.length);
+
+numbers.push(70);
+//console.log(numbers);
+console.log(numbers.length);
+
+numbers.pop();
+console.log(numbers.length);
+console.log(numbers);*/
